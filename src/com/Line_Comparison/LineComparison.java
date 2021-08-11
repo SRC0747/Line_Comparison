7ed35f769c91b23e5d90912e17e24873f3a9138f
SRC0747 <srcsrk1998@gmail.com>
2021-08-11 13:22:20 +0530

    Welcome to Line Comparison Problem.

---
+++ src/com/Line_Comparison/LineComparison.java
@@ -0,0 +1,8 @@
+package com.Line_Comparison;
+
+public class LineComparison {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Line-Comparison problem.");
+    }
+}
